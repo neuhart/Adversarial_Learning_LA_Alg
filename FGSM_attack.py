985a32833e1ebee6000b0db12bcd15480d177226
neuhart <philipneuhart@live.at>
2022-03-24 18:54:12 +0100

    Create FGSM_attack.py

---
+++ FGSM_attack.py
@@ -0,0 +1,30 @@
+import cleverhans
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import numpy as np
+
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]) #convert PIL image into tensor and normalize
+
+batch_size = 4 #number of samples per batch
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=True, transform=transform)
+#download training set, store into ./data and apply transform
+
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                          shuffle=True) #load in training set
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=True, transform=transform)
+#download test set, store into ./data and apply transform
+
+testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                         shuffle=False) #load in test set
+
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
