8e0d335e3f96be168a2006730959587a19129e73
neuhart <philipneuhart@live.at>
2022-04-13 20:11:12 +0200

    Update ResNet50.py

--- ResNet50.py
+++ ResNet50.py
@@ -5,6 +5,7 @@ import torchvision
 import torchvision.transforms as transforms
 import time
 import Lookahead_tutorial
+import load_data
 
 """
 https://pytorch.org/hub/pytorch_vision_resnet/
@@ -14,36 +15,16 @@ https://pytorch.org/tutorials/beginner/transfer_learning_tutorial.html (image tr
 batch_size = 50  # number of samples per batch
 nb_epochs = 5
 
-
-def ld_cifar10():
-    """Load training and test data."""
-    transform = transforms.Compose([
+transform = transforms.Compose([
             transforms.RandomResizedCrop(224),
             transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ])  # convert PIL image into tensor
 
-
-    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                            download=True, transform=transform)
-    # download training set, store into ./data and apply transform
-
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
-                                              shuffle=True, num_workers=4)  # load in training set
-
-    testset = torchvision.datasets.CIFAR10(root='./data', train=False,
-                                           download=True, transform=transform)
-    # download test set, store into ./data and apply transform
-
-    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
-                                             shuffle=False, num_workers=4)  # load in test set
-
-    return EasyDict(train=trainloader, test=testloader)
-
-def main():
+if __name__  == "__main__":
     # Load training and test data
-    data = ld_cifar10()
+    data = load_data.ld_cifar10(transform=transform, batch_size=batch_size)
     # Instantiate model, loss, and optimizer for training
     net = torchvision.models.resnet50()
     device = "cuda" if torch.cuda.is_available() else "cpu" # check if gpu is available
@@ -74,5 +55,3 @@ def main():
             )
         )
 
-if __name__  == "__main__":
-    main()
