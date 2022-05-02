cd6c79293bb94e447bd1a60c7ae7304a46356086
neuhart <91024138+neuhart@users.noreply.github.com>
2022-05-02 20:04:45 +0200

    Create optim_eval.py

---
+++ optim_eval.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+filename = "data/results.csv"
+
+df = pd.read_csv(filename)
+
+for col in df.columns:
+    plt.plot(range(df.shape[0]),df[col])
+plt.xlabel('Epochs')
+plt.legend(df.columns)
+plt.show()
+
