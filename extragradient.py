0ca7d575c79f7376b9481e87062cbaa933c16c09
neuhart <91024138+neuhart@users.noreply.github.com>
2022-05-06 19:39:04 +0200

    fixed error when calling super().init for ExtraSGD

--- extragradient.py
+++ extragradient.py
@@ -155,7 +155,7 @@ class ExtraSGD(Extragradient):
         super(ExtraSGD, self).__init__(params, defaults)
 
     def __setstate__(self, state):
-        super(SGD, self).__setstate__(state)
+        super(ExtraSGD, self).__setstate__(state)
         for group in self.param_groups:
             group.setdefault('nesterov', False)
 
