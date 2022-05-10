4435dc57f93e0f7727f4504492efde3ee9f2873a
neuhart <91024138+neuhart@users.noreply.github.com>
2022-05-10 23:21:10 +0200

    Update optim_eval.py

--- optim_eval.py
+++ optim_eval.py
@@ -2,7 +2,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # filename = "data/results.csv"
-filename = "data/adv_results.csv"
+# filename = "data/adv_results.csv"
+filename = "data/MNIST-adv_results.csv"
 df = pd.read_csv(filename)
 
 for col in df.columns:
