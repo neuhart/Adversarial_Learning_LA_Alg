d999e2e3c316d4b6bcfffec2e18ffe6e57c8ed9c
neuhart <91024138+neuhart@users.noreply.github.com>
2022-05-10 23:57:12 +0200

    Update optim_eval.py

--- optim_eval.py
+++ optim_eval.py
@@ -7,7 +7,7 @@ filename = "data/MNIST-adv_results.csv"
 df = pd.read_csv(filename)
 
 for col in df.columns:
-    plt.plot(range(df.shape[0]), df[col])
+    plt.plot(range(1,df.shape[0]+1), df[col])
 plt.xlabel('Epochs')
 plt.ylabel('Training Loss')
 plt.legend(df.columns)
