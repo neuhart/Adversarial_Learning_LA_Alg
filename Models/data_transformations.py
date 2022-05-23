5e8c291023a77b1d981b7fed4d281d20ed831ff7
neuhart <91024138+neuhart@users.noreply.github.com>
2022-05-23 18:41:35 +0200

    added model directory for better overview

---
+++ Models/data_transformations.py
@@ -0,0 +1,16 @@
+import torchvision.transforms as transforms
+
+
+def resnet_transform():
+    """https://pytorch.org/tutorials/beginner/transfer_learning_tutorial.html (image transformation for resnet)"""
+    return transforms.Compose([
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ])  # convert PIL image into tensor and transform to match ResNet50 requirements (see 1))
+
+
+def standard_transform():
+    """Basic Transformation which just converts the data into torch tensors"""
+    return transforms.Compose([transforms.ToTensor()])
--- main_project.py
+++ main_project.py
@@ -1,50 +1,20 @@
 import torch
-import torchvision
 from Optimizer import extragradient, Lookahead, OGDA
 import project_utils
 import data_utils
 from absl import app
-import torch.nn.functional as F
-import torch.nn as nn
-
+from Models import models, data_transformations
 """
 1) https://pytorch.org/hub/pytorch_vision_resnet/
 """
 
 
-class BasicCNN(torch.nn.Module):
-    """Basic CNN architecture."""
-
-    def __init__(self, in_channels=1):
-        super(BasicCNN, self).__init__()
-        self.conv1 = nn.Conv2d(
-            in_channels, 64, 8, 1
-        )  # (batch_size, 3, 28, 28) --> (batch_size, 64, 21, 21)
-        self.conv2 = nn.Conv2d(
-            64, 128, 6, 2
-        )  # (batch_size, 64, 21, 21) --> (batch_size, 128, 8, 8)
-        self.conv3 = nn.Conv2d(
-            128, 128, 5, 1
-        )  # (batch_size, 128, 8, 8) --> (batch_size, 128, 4, 4)
-        self.fc1 = nn.Linear(
-            128 * 4 * 4, 128
-        )  # (batch_size, 128, 4, 4) --> (batch_size, 2048)
-        self.fc2 = nn.Linear(128, 10)  # (batch_size, 128) --> (batch_size, 10)
-
-    def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
-        x = F.relu(self.conv3(x))
-        x = x.view(-1, 128 * 4 * 4)
-        x = self.fc1(x)
-        x = self.fc2(x)
-        return x
-
 def main(_):
     # net = torchvision.models.resnet50()  # 1)
-    net = BasicCNN()
+    # data = data_utils.ld_dataset(transform=data_transformations.resnet_transform())
 
-    data = data_utils.ld_dataset(model=net)
+    net = models.BasicCNN()
+    data = data_utils.ld_dataset(transform=data_transformations.standard_transform())
 
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
     net.to(device)  # transfers to gpu if available
