aeeba9c3955d20480dbcb330361c0f6fea586609
neuhart <91024138+neuhart@users.noreply.github.com>
2022-05-31 12:20:11 +0200

    Update MNIST_main.py

--- MNIST_main.py
+++ MNIST_main.py
@@ -8,7 +8,7 @@ from Models import models, data_transformations
 """
 
 
-def main(_):
+def main(optim):
     data_utils.code_settings()  # specify general settings
 
     data = data_utils.ld_dataset(dataset_name='MNIST', transform=data_transformations.standard_transform())
