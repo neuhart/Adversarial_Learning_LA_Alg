d68fee24a5d1a411db93e857a86118aad4fcc3c8
neuhart <91024138+neuhart@users.noreply.github.com>
2022-05-31 10:51:23 +0200

    Update extragradient.py

--- Optimizer/extragradient.py
+++ Optimizer/extragradient.py
@@ -94,98 +94,56 @@ class Extragradient(Optimizer):
 
 
 class ExtraSGD(Extragradient):
-    """Implements stochastic gradient descent with extrapolation step (optionally with momentum).
-
-    Nesterov momentum is based on the formula from
-    `On the importance of initialization and momentum in deep learning`__.
-
-    Args:
+    """Implements the SGD Algorithm with extrapolation step
+    Arguments:
         params (iterable): iterable of parameters to optimize or dicts defining
             parameter groups
-        lr (float): learning rate
-        momentum (float, optional): momentum factor (default: 0)
-        weight_decay (float, optional): weight decay (L2 penalty) (default: 0)
-        dampening (float, optional): dampening for momentum (default: 0)
-        nesterov (bool, optional): enables Nesterov momentum (default: False)
-
-    Example:
-        optimizer = torch.optim.ExtraSGD(model.parameters(), lr=0.1, momentum=0.9)
-        optimizer.zero_grad()
-        loss_fn(model(input), target).backward()
-        optimizer.extrapolation()
-        optimizer.zero_grad()
-        loss_fn(model(input), target).backward()
-        optimizer.step()
-
-    __ http://www.cs.toronto.edu/%7Ehinton/absps/momentum.pdf
-
-    .. note::
-        The implementation of SGD with Momentum/Nesterov subtly differs from
-        Sutskever et. al. and implementations in some other frameworks.
-
-        Considering the specific case of Momentum, the update can be written as
-
-        .. math::
-                  v = \rho * v + g \\
-                  p = p - lr * v
-
-        where p, g, v and :math:`\rho` denote the parameters, gradient,
-        velocity, and momentum respectively.
-
-        This is in contrast to Sutskever et. al. and
-        other frameworks which employ an update of the form
-
-        .. math::
-             v = \rho * v + lr * g \\
-             p = p - v
-
-        The Nesterov version is analogously modified.
+        lr (float, optional): learning rate (default: 1e-3)
+        momentum (float, optional): momentum parameter (default: 0)
     """
-    def __init__(self, params, lr=required, momentum=0, dampening=0,
-                 weight_decay=0, nesterov=False):
-        if lr is not required and lr < 0.0:
+    def __init__(self, params, lr=1e-3, momentum=0):
+        if not 0.0 <= lr:
             raise ValueError("Invalid learning rate: {}".format(lr))
         if momentum < 0.0:
             raise ValueError("Invalid momentum value: {}".format(momentum))
-        if weight_decay < 0.0:
-            raise ValueError("Invalid weight_decay value: {}".format(weight_decay))
 
-        defaults = dict(lr=lr, momentum=momentum, dampening=dampening,
-                        weight_decay=weight_decay, nesterov=nesterov)
-        if nesterov and (momentum <= 0 or dampening != 0):
-            raise ValueError("Nesterov momentum requires a momentum and zero dampening")
+        defaults = dict(lr=lr, momentum=momentum)
         super(ExtraSGD, self).__init__(params, defaults)
 
     def __setstate__(self, state):
         super(ExtraSGD, self).__setstate__(state)
-        for group in self.param_groups:
-            group.setdefault('nesterov', False)
 
     def update(self, p, group):
-        weight_decay = group['weight_decay']
-        momentum = group['momentum']
-        dampening = group['dampening']
-        nesterov = group['nesterov']
-
         if p.grad is None:
             return None
-        d_p = p.grad.data
-        if weight_decay != 0:
-            d_p.add_(weight_decay, p.data)
-        if momentum != 0:
-            param_state = self.state[p]
-            if 'momentum_buffer' not in param_state:
-                buf = param_state['momentum_buffer'] = torch.zeros_like(p.data)
-                buf.mul_(momentum).add_(d_p)
-            else:
-                buf = param_state['momentum_buffer']
-                buf.mul_(momentum).add_(1 - dampening, d_p)
-            if nesterov:
-                d_p = d_p.add(momentum, buf)
-            else:
-                d_p = buf
+        grad = p.grad  # compute grad
 
-        return -group['lr']*d_p
+        state = self.state[p]
+
+        # State initialization: check if momentum was already initialized
+        if 'momentum_buffer' not in state:
+            # not init.
+            buf = None
+        else:
+            # init. and saved in momentum_buffer
+            buf = state['momentum_buffer']
+
+        momentum = group['momentum']
+
+        if momentum != 0:
+            if buf is None:
+                # if momentum buffer is not initialized yet, start with grad
+                buf = torch.clone(grad).detach()
+                state['momentum_buffer'] = buf
+            else:
+                # if already init. multiply with momentum-weight and add grad
+                buf.mul_(momentum).add_(grad, alpha=1)
+                state['momentum_buffer'] = buf
+            d_p = buf
+        else:
+            d_p = grad
+
+        return d_p.mul_(-group['lr'])
 
 
 class ExtraAdam(Extragradient):
