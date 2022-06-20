11ceeebb8610df7b8280e58a334c1e220c9b8af2
neuhart <91024138+neuhart@users.noreply.github.com>
2022-06-20 18:56:37 +0200

    Update param_tuning.py

--- param_tuning.py
+++ param_tuning.py
@@ -15,7 +15,7 @@ from pathlib import Path
 
 
 def main():
-    settings = EasyDict(nb_epochs=1, adv_train=True)  # specify general settings
+    settings = EasyDict(nb_epochs=3, adv_train=False)  # specify general settings
     settings.device = torch.device(project_utils.query_int('Select GPU [0,3]:')) if \
         torch.cuda.is_available() else torch.device('cpu')
 
@@ -27,7 +27,7 @@ def main():
     scores = pd.DataFrame()
 
     for optim in inner_optims_list:
-        for lr in [1e-3, 3*1e-3, 1e-4]:
+        for lr in [1e-3, 3e-3, 1e-4]:
             settings.lr = lr
             if optim[:3] == 'LA-':  # check if Lookahead is used
                 settings.LA = True
