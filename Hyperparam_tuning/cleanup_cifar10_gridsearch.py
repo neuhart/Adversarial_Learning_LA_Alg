2d686a8d6b3ed5bdbfbcc0ee54906a7b2ca91da5
neuhart <91024138+neuhart@users.noreply.github.com>
2022-07-04 12:55:53 +0200

    Update cleanup_cifar10_gridsearch.py

--- Hyperparam_tuning/cleanup_cifar10_gridsearch.py
+++ Hyperparam_tuning/cleanup_cifar10_gridsearch.py
@@ -10,6 +10,25 @@ train_path = "{}/adv_train_results".format(dataset) if adv_train else "{}/clean_
 optims = ['SGD', 'Adam', 'OGDA', 'ExtraSGD', 'ExtraAdam']
 
 for file in os.listdir(train_path):
+    df = pd.read_csv(test_path + "/" + file)
+
+    if file.startswith('Lookahead-Adam'):
+        'Do nothing'
+    else:
+        dfnew = df.iloc[1]
+        dfnew.transpose()
+        """
+        dfnew = pd.DataFrame()
+        dfold = pd.DataFrame()
+        dfnew = pd.concat([dfnew,df.iloc[1,:]],axis=1)
+        dfold = pd.concat([dfold, df.iloc[0,:]], axis=1)
+        """
+        dfnew.to_csv(test_path + '/' + file, index=False)
+        dfold.to_csv(
+            "C:/Users/phili/OneDrive/Desktop/Uni_Philip/Master Mathematik/Masterarbeit/GitHub/CIFAR10_gridsearch_smallnetwork_backup" + '/' + file,
+            index=False)
+    break
+    """
     df = pd.read_csv(train_path + "/" + file)
     if file.startswith('Lookahead'):
         if file.startswith('Lookahead-Adam'):
@@ -24,4 +43,4 @@ for file in os.listdir(train_path):
         dfold = df.iloc[:, :5]
         dfnew.to_csv(train_path + '/' + file, index=False)
         dfold.to_csv("C:/Users/phili/OneDrive/Desktop/Uni_Philip/Master Mathematik/Masterarbeit/GitHub/CIFAR10_gridsearch_smallnetwork_backup" + '/' + file, index=False)
-    break
+    """
